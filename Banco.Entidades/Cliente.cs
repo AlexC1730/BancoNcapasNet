0c0fbb8f6092baa8db57c5d77b44162305460440
christiancondori <christiancondorivargas144@gmail.com>
2023-02-17 18:56:11 -0500

    se agrego las Entidades Cliente, TipoCliente y TipoDocumento

---
+++ Banco.Entidades/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace Banco.Entidades
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string RazonSocial { get; set; }
+        public string NumeroDocumento { get; set; }
+        public int IDTipoCliente { get; set; }
+        public int IDTipoDocumento { get; set; }
+        public string Direccion { get; set; }
+        public string Referencia { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+
+    }
+}
